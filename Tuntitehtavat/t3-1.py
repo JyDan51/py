bd40edef39864d3f7f93945859491caa604abcd6
JyDan51 <152958914+JyDan51@users.noreply.github.com>
2026-08-26 14:19:01 +0300

    TT (if, elif, else)

---
+++ Tuntitehtavat/t3-1.py
@@ -0,0 +1,15 @@
+print ("Hello")
+vuo = int (input("Anna vuosi: ") )
+
+
+if vuo % 2 == 0:
+    print("Oli olympiavuosi")
+elif vuo == 2020:
+    print("Ei ollut olympialaisia, mutta 2021 oli (korona syyst!")    
+elif vuo == 2021:
+    print("Oli olympiavuosi")    
+else:
+    print("Ei ole olympriavuosi")
+
+
+input("Press..")
